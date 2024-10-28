94d68780b47e8d7c54236d10fd216d5a434ca479
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-10-28 12:25:10 +0200

    first commit

---
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/QuizzerappApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.quizzerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzerappApplication.class, args);
+	}
+
+}
---
+++ quizzerapp/src/test/java/fi/haagahelia/quizzerapp/QuizzerappApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.quizzerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
