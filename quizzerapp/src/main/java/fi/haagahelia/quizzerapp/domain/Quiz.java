abfafa2c9031c236a5568366200621524c021f61
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-10-31 16:17:05 +0200

    Added id, name and description to Quiz class

--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/Quiz.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/Quiz.java
@@ -1,5 +1,17 @@
 package fi.haagahelia.quizzerapp.domain;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String description;
 
 }
