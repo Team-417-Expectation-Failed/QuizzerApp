3931a4d1f4762d624cc80bfac35e9fa7810ad808
Heli Kulomaa <heli.kulomaa@gmail.com>
2024-11-04 20:10:05 +0200

    Editing quizzes functionality and links from homepage to adding and back

--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/QuizRepository.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/QuizRepository.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 @Repository
-public interface QuizRepository extends JpaRepository<Quiz, Long>{
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
     List<Quiz> findAll();
 }
+
+// public interface QuizRepository extends CrudRepository<Quiz, Long> {
+
+// List<Quiz> findAll();
+
+// }
--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/web/QuizzerAppController.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/web/QuizzerAppController.java
@@ -3,7 +3,9 @@ package fi.haagahelia.quizzerapp.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,4 +42,27 @@ public class QuizzerAppController {
     public Iterable<Quiz> getQuizzes() {
         return quizRepository.findAll();
     }
+
+    // Edit quiz
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateQuizForm(@PathVariable("id") long id, Model model) {
+        Quiz quiz = quizRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid quiz ID:" + id));
+        model.addAttribute("quiz", quiz);
+        return "editquiz";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateQuiz(@PathVariable("id") long id, Quiz quiz,
+            BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            quiz.setId(id);
+            return "editquiz";
+        }
+
+        quizRepository.save(quiz);
+        return "redirect:/";
+    }
+
 }
