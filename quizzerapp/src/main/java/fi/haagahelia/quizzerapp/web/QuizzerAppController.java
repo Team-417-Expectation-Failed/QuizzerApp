78dcc7da0f548837a44fd9b513eefaee72977a2d
Kaito Stenroos <kaito.stenroos@outlook.com>
2024-11-04 12:52:37 +0200

    Add quiz template added

--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/web/QuizzerAppController.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/web/QuizzerAppController.java
@@ -3,6 +3,7 @@ package fi.haagahelia.quizzerapp.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,7 +18,8 @@ public class QuizzerAppController {
     private QuizRepository quizRepository;
 
     @RequestMapping(value = "/homepage")
-    public String homePage() {
+    public String homePage(Model model) {
+        model.addAttribute("quiz", new Quiz());
         return "homepage";
     }
 
