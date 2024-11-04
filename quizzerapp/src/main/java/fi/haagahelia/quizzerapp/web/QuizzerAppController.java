3f32d993af66c0de9743b5f9163b2346f6867b21
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-11-04 11:41:00 +0200

    Fixed QuizAppController

--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/web/QuizzerAppController.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/web/QuizzerAppController.java
@@ -1,9 +1,27 @@
 package fi.haagahelia.quizzerapp.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import fi.haagahelia.quizzerapp.domain.QuizRepository;
 
+@Controller
 public class QuizzerAppController {
-        public static void main(String[] args) {
-            SpringApplication.run(QuizzerAppController.class, args);
-        }
+
+    @Autowired
+    private QuizRepository quizRepository;
+
+    @RequestMapping(value = "/homepage")
+    public String homePage() {
+        return "homepage";
+    }
+
+    @GetMapping("/quizzes")
+    @ResponseBody
+    public Iterable<Quiz> getQuizzes() {
+        return quizRepository.findAll();
+    }
 }
