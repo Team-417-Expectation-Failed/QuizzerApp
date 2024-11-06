95ea02e014cfa4505a4ec836d7def7288f728307
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-11-06 13:38:50 +0200

    Add AnswerOptionRepository interface extending JpaRepository

--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/AnswerOptionRepository.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/AnswerOptionRepository.java
@@ -1,5 +1,7 @@
 package fi.haagahelia.quizzerapp.domain;
 
-public interface AnswerOptionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerOptionRepository extends JpaRepository<AnswerOption, Long> {
 
 }
