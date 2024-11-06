6856656a5bc60fc0b76671844b66b64c0a79277e
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-11-06 13:22:20 +0200

    Added @JoinColumn referencing question_id, empty default constructior and parameterized constructor

--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/AnswerOption.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/AnswerOption.java
@@ -3,6 +3,7 @@ package fi.haagahelia.quizzerapp.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -13,7 +14,17 @@ public class AnswerOption {
     private String answerOption;
     private boolean correct;
     @ManyToOne(optional = false)
+    @JoinColumn(name = "question_id")
     private Question belongsToQuestion;
+
+    public AnswerOption() {
+    }
+
+    public AnswerOption(String answerOption, boolean correct, Question belongsToQuestion) {
+        this.answerOption = answerOption;
+        this.correct = correct;
+        this.belongsToQuestion = belongsToQuestion;
+    }
 
     public Long getId() {
         return id;
