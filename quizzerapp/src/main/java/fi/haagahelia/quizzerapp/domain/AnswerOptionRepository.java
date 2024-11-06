dc75e97b07dceed0f38c88936c93d85d68931d63
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-11-06 18:21:51 +0200

    Add @Repository annotation to AnswerOptionRepository

--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/AnswerOptionRepository.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/AnswerOptionRepository.java
@@ -1,7 +1,9 @@
 package fi.haagahelia.quizzerapp.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnswerOptionRepository extends JpaRepository<AnswerOption, Long> {
 
 }
