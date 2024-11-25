50613019748296b834c58f04a0f741613433b74f
Heli Kulomaa <heli.kulomaa@gmail.com>
2024-11-25 12:03:39 +0200

    beginning of error handling, not ready

--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/web/QuizzerRestController.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/web/QuizzerRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import fi.haagahelia.quizzerapp.dto.QuestionDTO;
 import fi.haagahelia.quizzerapp.dto.QuizDTO;
@@ -37,14 +38,14 @@ public class QuizzerRestController {
         List<Quiz> quizzes = quizService.findAllPublishedQuizzes();
 
         List<QuizDTO> quizDTOs = quizzes.stream()
-            .map(quiz -> new QuizDTO(
-                    quiz.getId(),
-                    quiz.getName(),
-                    quiz.getDescription(),
-                    quiz.getCreatedDate(),
-                    quiz.isPublished(),
-                    quiz.getQuizCategory().getName()))
-            .collect(Collectors.toList());
+                .map(quiz -> new QuizDTO(
+                        quiz.getId(),
+                        quiz.getName(),
+                        quiz.getDescription(),
+                        quiz.getCreatedDate(),
+                        quiz.isPublished(),
+                        quiz.getQuizCategory().getName()))
+                .collect(Collectors.toList());
 
         if (quizDTOs.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); // Return 404 if no quizzes found
@@ -53,20 +54,32 @@ public class QuizzerRestController {
         return ResponseEntity.ok(quizDTOs); // Return HTTP 200 with quizzes
     }
 
+    // Get quiz by id using ResponseEntity to handle exceptions
+    @GetMapping("/quizzes/{quizId}")
+    public Quiz findQuizById(@PathVariable Long quizId) {
+        Quiz quiz = quizService.findQuizById(quizId); // Returns quiz or null
+        if (quiz != null) {
+            return quiz; // HTTP 200
+        } else {
+            String errorMessage = "Quiz not found with ID: " + quizId;
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage); // HTTP 404
+        }
+    }
+
     // Get questions by quiz id
     @GetMapping("/quizzes/{quizId}/questions")
     public ResponseEntity<List<QuestionDTO>> findQuestionsByQuizId(@PathVariable Long quizId) {
         List<Question> questions = questionService.findQuestionsByQuizId(quizId);
 
         List<QuestionDTO> questionDTOs = questions.stream()
-            .map(question -> new QuestionDTO(
-                    question.getId(),
-                    question.getQuestionBody(),
-                    question.getDifficultyLevel(),
-                    question.getAnswerOptions().stream()
-                            .map(answerOption -> answerOption.getAnswerOptionBody())
-                            .collect(Collectors.toList())))
-            .collect(Collectors.toList());
+                .map(question -> new QuestionDTO(
+                        question.getId(),
+                        question.getQuestionBody(),
+                        question.getDifficultyLevel(),
+                        question.getAnswerOptions().stream()
+                                .map(answerOption -> answerOption.getAnswerOptionBody())
+                                .collect(Collectors.toList())))
+                .collect(Collectors.toList());
 
         if (questionDTOs.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); // Return 404 if no questions found
