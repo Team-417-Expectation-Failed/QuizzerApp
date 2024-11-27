4f36bd58240367e02860e5b32c1b6c29c25e41b2
Jehu <jarppa9900@hotmail.fi>
2024-11-27 16:42:45 +0200

    Deleted Cascade import

--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/QuizCategory.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/domain/QuizCategory.java
@@ -3,7 +3,6 @@ package fi.haagahelia.quizzerapp.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
