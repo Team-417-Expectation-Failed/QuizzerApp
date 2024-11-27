cc3c85c88f918b3d777735b3b4669b8aed2d731c
Heli Kulomaa <heli.kulomaa@gmail.com>
2024-11-27 17:21:20 +0200

    Remove v1 from api url in front end side

--- frontend/src/quizapi.js
+++ frontend/src/quizapi.js
@@ -1,22 +1,22 @@
-const apiUrl = import.meta.env.VITE_API_URL + '/v1';
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export function getPublishedQuizzes() {
   return fetch(`${apiUrl}/quizzes`)
-  .then(response =>{
-    if (!response.ok) 
+    .then(response => {
+      if (!response.ok)
         throw new Error("Error in fetch" + response.statusText);
-        return response.json();
-    
- })  
+      return response.json();
+
+    })
 }
 
 export function getQuizQuestions(quizId) {
   return fetch(`${apiUrl}/quizzes/${quizId}/questions`)
-  .then(response =>{
-    if (!response.ok) 
+    .then(response => {
+      if (!response.ok)
         throw new Error("Error in fetch" + response.statusText);
-        console.log(response);
-        return response.json();
-    
- })  
+      console.log(response);
+      return response.json();
+
+    })
 }
