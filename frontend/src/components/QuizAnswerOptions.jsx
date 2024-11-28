2130cfb95ee137b15f7089c1b5366fcb52ee9635
Kaito Stenroos <kaito.stenroos@outlook.com>
2024-11-28 16:16:03 +0200

    Make radio buttons selectable only one at answeroption a time

--- frontend/src/components/QuizAnswerOptions.jsx
+++ frontend/src/components/QuizAnswerOptions.jsx
@@ -1,24 +1,33 @@
+import { useState } from 'react';
 import { FormControl, Radio, RadioGroup, FormControlLabel } from '@mui/material';
 
 function QuizAnswerOptions({answerOptions}) {
+    const [value, setValue] = useState(''); 
+
+    const handleRadioChange = (event) => {
+        setValue(event.target.value);
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-    }
+    };
 
     console.log(answerOptions);
 
     return (
     <form onSubmit={handleSubmit}>
-        <FormControl>
-            {answerOptions.map((answerOption) => (
-                <RadioGroup
-                key={answerOption.id}
-            >
-                <FormControlLabel  value={answerOption.id} control={<Radio />} label={answerOption.answerOptionBody} />
-            </RadioGroup>
-            ))}
-        </FormControl>
+        <FormControl variant='standard'>
+                <RadioGroup value={value} onChange={handleRadioChange}>
+                    {answerOptions.map((answerOption) => (
+                        <FormControlLabel
+                            key={answerOption.id}
+                            value={answerOption.id}
+                            control={<Radio />}
+                            label={answerOption.answerOptionBody}
+                        />
+                    ))}
+                </RadioGroup>
+            </FormControl>
     </form>
     )
 }
