6841440cf7f2a90cd4ea11d30d4431a060c7f559
Kaito Stenroos <kaito.stenroos@outlook.com>
2024-12-05 13:11:27 +0200

    Add snackbar component for informing student if answer is correct

--- frontend/src/components/QuizAnswerOptions.jsx
+++ frontend/src/components/QuizAnswerOptions.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { FormControl, Radio, RadioGroup, FormControlLabel } from '@mui/material';
+import { FormControl, Radio, RadioGroup, FormControlLabel, Snackbar, Button } from '@mui/material';
 
 function QuizAnswerOptions({answerOptions}) {
-    const [value, setValue] = useState(''); 
+    const [value, setValue] = useState('');
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState('');
 
     const handleRadioChange = (event) => {
         setValue(event.target.value);
@@ -10,25 +12,46 @@ function QuizAnswerOptions({answerOptions}) {
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        const selectedOption = answerOptions.find(option => option.id === parseInt(value)).correct;
+        if (selectedOption) {
+            setMessage("That is correct, good job!");
+        } else {
+            setMessage("That is not correct, try again!");
+        }
+        setOpen(true);
     };
 
-    console.log(answerOptions);
+    const handleClose = () => {
+        setOpen(false);
+    };
+
 
     return (
-    <form onSubmit={handleSubmit}>
-        <FormControl variant='standard'>
-                <RadioGroup value={value} onChange={handleRadioChange}>
-                    {answerOptions.map((answerOption) => (
-                        <FormControlLabel
-                            key={answerOption.id}
-                            value={answerOption.id}
-                            control={<Radio />}
-                            label={answerOption.answerOptionBody}
-                        />
-                    ))}
-                </RadioGroup>
-            </FormControl>
-    </form>
+        <>
+            <form onSubmit={handleSubmit}>
+                <FormControl variant='standard'>
+                    <RadioGroup value={value} onChange={handleRadioChange}>
+                        {answerOptions.map((answerOption) => (
+                            <FormControlLabel
+                                key={answerOption.id}
+                                value={answerOption.id}
+                                control={<Radio />}
+                                label={answerOption.answerOptionBody}
+                            />
+                        ))}
+                    </RadioGroup>
+                    <Button type="submit" variant="contained" color="primary">
+                        Submit
+                    </Button>
+                </FormControl>
+            </form>
+            <Snackbar
+                open={open}
+                message={message}
+                autoHideDuration={3000}
+                onClose={handleClose}
+            />
+        </>
     )
 }
 
