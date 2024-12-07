90079715162c2aa87453a65606913db8825becda
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-12-07 19:10:10 +0200

    Enhance AnswerRepository to use custom query for retrieving answers by quiz ID to avoid refactoring

--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/repositories/AnswerRepository.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/repositories/AnswerRepository.java
@@ -5,9 +5,12 @@ import fi.haagahelia.quizzerapp.domain.Answer;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    List<Answer> findByQuizId(Long quizId);
+    @Query("SELECT a FROM Answer a JOIN a.answerOption ao JOIN ao.question q JOIN q.quiz qz WHERE qz.id = :quizId")
+    List<Answer> findByQuizId(@Param("quizId") Long quizId);
 }
