ba369a8db654a2569982b526a4b02149be7c9498
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-12-07 18:43:05 +0200

    Refactor AnswerDTO by removing unused code

--- quizzerapp/src/main/java/fi/haagahelia/quizzerapp/dto/AnswerDTO.java
+++ quizzerapp/src/main/java/fi/haagahelia/quizzerapp/dto/AnswerDTO.java
@@ -3,31 +3,13 @@ package fi.haagahelia.quizzerapp.dto;
 import jakarta.validation.constraints.NotNull;
 
 public class AnswerDTO {
-
-    @NotNull
-    private Long questionId;
     @NotNull
     private Long answerOptionId;
-    @NotNull
-    private Long quizId;
-    private Boolean correct;
 
-    public AnswerDTO() {
-    }
+    public AnswerDTO() {}
 
-    public AnswerDTO(Long questionId, Long answerOptionId, Long quizId) {
-        this.questionId = questionId;
+    public AnswerDTO(Long answerOptionId) {
         this.answerOptionId = answerOptionId;
-        this.quizId = quizId;
-        this.correct = false;
-    }
-
-    public Long getQuestionId() {
-        return questionId;
-    }
-
-    public void setQuestionId(Long questionId) {
-        this.questionId = questionId;
     }
 
     public Long getAnswerOptionId() {
@@ -36,31 +18,5 @@ public class AnswerDTO {
 
     public void setAnswerOptionId(Long answerOptionId) {
         this.answerOptionId = answerOptionId;
-    }
-
-    public Long getQuizId() {
-        return quizId;
-    }
-
-    public void setQuizId(Long quizId) {
-        this.quizId = quizId;
-    }
-
-    public Boolean getCorrect() {
-        return correct;
-    }
-
-    public void setCorrect(Boolean correct) {
-        this.correct = correct;
-    }
-
-    @Override
-    public String toString() {
-        return "AnswerDTO{" +
-                "questionId=" + questionId +
-                ", answerOptionId=" + answerOptionId +
-                ", quizId=" + quizId +
-                ", correct=" + correct +
-                '}';
     }
 }
